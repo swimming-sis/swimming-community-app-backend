7c04ffa13b3966e010352c30c81b4269b358cee2
gahyun <gmeoqq@gmail.com>
2023-09-06 15:11:50 +0900

    first commit

---
+++ src/main/java/com/swimmingcommunityapp/SwimmingCommunityAppApplication.java
@@ -0,0 +1,13 @@
+package com.swimmingcommunityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingCommunityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingCommunityAppApplication.class, args);
+	}
+
+}
