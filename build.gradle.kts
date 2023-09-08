348c06179d80bc98106927188d796fee97a0b950
gahyun <gmeoqq@gmail.com>
2023-09-09 03:57:11 +0900

    [chore] : swagger 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,12 +22,26 @@ repositories {
 }
 
 dependencies {
+	//jpa
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java")
+
+	//swagger
+	implementation ("io.springfox:springfox-boot-starter:3.0.0")
+	implementation ("io.springfox:springfox-swagger-ui:3.0.0")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	//security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation ("javax.xml.bind:jaxb-api:2.3.0")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/swimmingcommunityapp/configuration/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.swimmingcommunityapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swimming-community-app")
+                .description("수영인들을 위한 커뮤니티, 일지기록을 위한 사이트")
+                .version("1.0")
+                .build();
+    }
+}
