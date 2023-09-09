7796591a5e592a06af047513d0044e86593c5a04
gahyun <gmeoqq@gmail.com>
2023-09-09 16:48:50 +0900

    [feat] : swagger에 security추가

--- src/main/java/com/swimmingcommunityapp/configuration/SwaggerConfig.java
+++ src/main/java/com/swimmingcommunityapp/configuration/SwaggerConfig.java
@@ -7,7 +7,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +19,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -26,7 +32,26 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("Swimming-community-app")
                 .description("수영인들을 위한 커뮤니티, 일지기록을 위한 사이트")
+                .contact(new Contact("KAHYUN", "https://velog.io/@gmeoq", "gmeoqq@gmail.com"))
                 .version("1.0")
                 .build();
     }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
+
