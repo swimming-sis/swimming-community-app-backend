ac5a5ef92db664ba3a19108db24925759608e020
gahyun <gmeoqq@gmail.com>
2023-09-13 12:22:08 +0900

    [test] : test 코드 삭제

---
+++ src/test/java/com/swimmingcommunityapp/SwimmingCommunityAppApplicationTests.java
@@ -0,0 +1,9 @@
+package com.swimmingcommunityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimmingCommunityAppApplicationTests {
+
+}
