dfb046e9f7f6f7c4284cf8e7c1931393cb2ec4c8
gahyun <gmeoqq@gmail.com>
2023-09-25 00:22:17 +0900

    [build] : cors 설정 변경

--- src/main/java/com/swimmingcommunityapp/configuration/SecurityConfig.java
+++ src/main/java/com/swimmingcommunityapp/configuration/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedOrigin("https://upuhupuh.netlify.app");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
 //        corsConfiguration.setAllowCredentials(true);
